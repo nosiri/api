e90843198d26a1bf8d3ab91dbeb7b7e8c8570063
Shaygan Shokrollahi <shayganshok0@gmail.com>
2019-03-31 12:33:50 +0430

    :new: Custom 404 error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -43,8 +44,11 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
-    public function render($request, Exception $exception)
-    {
+    public function render($request, Exception $exception) {
+        //Custom 404 error
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json(['status' => false, 'result' => ['error' => 'Method not found']]);
+        }
         return parent::render($request, $exception);
     }
 }
