fe1a0a737a11f9f007ceebabf7b6b90ffc6c0b89
Shaygan Shokrollahi <shayganshok0@gmail.com>
2019-04-15 23:10:35 +0430

    :wrench: Fixed the "Class 'Tymon\JWTAuth\Providers\LumenServiceProvider' not found" error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -75,7 +75,6 @@ $app->singleton(
 
 // $app->register(App\Providers\AppServiceProvider::class);
  $app->register(App\Providers\AuthServiceProvider::class);
- $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
  $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 
